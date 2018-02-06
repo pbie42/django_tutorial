663eef8729848d913b4c3dfaf22f5aef31bab949
callmepaulb <paul.w.bie@gmail.com>
2018-02-06 16:27:50 +0100

    [Add] - Article model added wtih some test data in the db. Now displaying db data on the /articles page

---
+++ djangonautic/articles/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+	title = models.CharField(max_length=100)
+	slug = models.SlugField()
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+	# Add in thumbnail later
+	# Add in author later
+
+	def __str__(self):
+		return self.title
+
+
+# python manage.py makemigrations
+# python manage.py migrate
